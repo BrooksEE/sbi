41f815c5950347164262313ef0233be154aed051
Dennis Muhlestein <djmuhlestein@gmail.com>
2014-09-08 14:26:01 -0600

    Moving linux_mt programs to linux subdir.  Combine programs since they were the same.

--- examples/linux/main.c
+++ examples/linux/main.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <sbi.h>
+#include <stdbool.h>
 
 #include "funclib.h"
 
-FILE* f;
-
-int pos;
+FILE* f1;
 
 byte getfch(PCOUNT p, void* rt)
 {
-    fseek(f,p, SEEK_SET);
-	return fgetc(f); 
+	fseek (f1, p, SEEK_SET);
+	byte r = fgetc(f1);
+    //printf ( "prog1 byte %d=%d\n", p, r );
+	return r;
 }
 
 int main(int argc, char** argv)
 {
-	printf("\nSBI Runtime for Linux\n\n");
+	printf("\nSBI Multithreading Runtime for Linux\n\n");
 	
 	// Open executable
-	if (argc!=2) { printf("Wrong arguments\n\nUse:\n %s <program.sbi>\n", argv[0]); return 8; }
+	if (argc<2) { printf("Wrong arguments\n\nUse:\n %s <program.sbi> <-i>\n\n\t-i enable interrupt 2\n", argv[0]); return 8; }
 	printf("Loading %s...\n", argv[1]);
-	f = fopen((char*)argv[1], "rb");
+	f1 = fopen((char*)argv[1], "rb");
+	if (!f1) { printf("Can't open file %s\n", argv[1]); return 1; }
 	
-	if (!f) { printf("Can't open file!\n"); return 1; }
-	
-	// Init_
+	// Init
     sbi_context_t ctx;
     ctx.debugn=debugn;
     ctx.errorn=errorn;
@@ -32,26 +32,27 @@ int main(int argc, char** argv)
     ctx.sbi_user_funcs[0] = myfunc;
     ctx.sbi_user_funcs[1] = msgbox;
     ctx.sbi_user_funcs[2] = getnum;
-    
-
-	void* rt = sbi_init(&ctx); pos=0;
+	void* rt = sbi_init(&ctx);
 	
-	int ret = sbi_begin(rt);
-	if (ret==1) printf("Initialization error (no function pointers)\n");
-	if (ret==2) printf("Initialization error (old format version)\n");
-	if (ret==3) printf("Initialization error (invalid program file)\n");
-	if (ret>3) printf("Initialization error (unknown: %i)\n", ret);
-	
-	if (ret>0) return 1;
-	
+    int ret = sbi_begin(rt);
+    if (ret) {
+        printf ( "Program begin error: %d\n", ret );
+        return ret;
+    }
+     
 	printf("Running...\n");
 	
+    bool interrupts = argc>2 && strcmp(argv[2],"-i")==0;
+    int int_cnt=0;
 	while (sbi_running(rt)>0)
 	{
 		ret = sbi_step(rt);
+        if (interrupts && (++int_cnt % 100==0))
+            sbi_interrupt(2,rt);
 	}
+    sbi_cleanup(rt);
 	
-	fclose(f);
+	printf("All threads terminated\n");
 	
 	if (ret==1) printf("Program reached end (no exit found)\n");
 	if (ret==2) printf("Program exited (no errors)\n");
@@ -59,5 +60,7 @@ int main(int argc, char** argv)
 	if (ret==4) printf("Program exited (can't understand byte)\n");
 	if (ret==5) printf("Program exited (user error)\n");
 	
-	if (ret<2) return 0; else return 1;
+	fclose(f1);
+	
+	return (ret<2) ? 0 : 1;
 }
